e2d87dd9d6a72b2f82d4730658332e7fbcad064b
Yun <95ybya@gmail.com>
2022-02-11 21:56:34 +0900

    Creating and Combining Views

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 윤병은 on 2022/02/11.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
